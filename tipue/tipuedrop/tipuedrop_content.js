c59f990f2e2418eec1915a9b74991f5e39a76719
tonychyan <junqian@uw.edu>
2013-11-12 12:32:18 -0800

    Initial Commit

---
+++ tipue/tipuedrop/tipuedrop_content.js
@@ -0,0 +1,5 @@
+var tipuedrop = {"pages": [
+     {"title": "Tipue drop", "thumb": "img/drop.png", "text": "Tipue drop, a search suggestion box jQuery plugin", "tags": "JavaScript js", "loc": "http://www.tipue.com/drop"},
+     {"title": "Tipue Search", "thumb": "img/search.png", "text": "Tipue Search, a site search engine jQuery plugin", "tags": "JavaScript js", "loc": "http://www.tipue.com/search"}, 
+     {"title": "About Tipue", "thumb": "img/about.png", "text": "Tipue is a small web development studio based in North London", "tags": "", "loc": "http://www.tipue.com/about"}
+]};
